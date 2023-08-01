5a7a74162e6313d7ef47f25265c57cad4d2a5cff
Vesna Adjei <vadjei420@gmail.com>
2023-08-01 11:07:40 -0400

    Initial Commit

---
+++ Project/ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectApp.swift
+//  Project
+//
+//  Created by scholar on 8/1/23.
+//
+
+import SwiftUI
+
+@main
+struct ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
